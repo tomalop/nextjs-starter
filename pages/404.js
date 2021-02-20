749d69b26a165bc1ca7b73cd882782776f9acba1
Pavel Cacín <cacinpavel@gmail.com>
2021-02-20 14:14:48 +0100

    Added 404 page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <>
+      <h1>404 - Page Not Found</h1>
+      <Link href="/">
+        <a>Go back home</a>
+      </Link>
+    </>
+  );
+};
+
+export default Home;
