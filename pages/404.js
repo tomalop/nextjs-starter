ac7d00a06bd21ff3407ba63c633600f2742d29dc
Pavel Cacín <cacinpavel@gmail.com>
2021-02-20 14:53:40 +0100

    Removed redundant files

--- pages/404.js
+++ pages/404.js
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <h1>404 - Page Not Found</h1>
       <Link href="/">
         <a>Go back home</a>
       </Link>
-    </>
+    </div>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
-import '../styles/reset.css';
-import '../styles/main.css';
+import "../styles/main.css";
+import Head from "next/head";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <title>Tomalop Blog</title>
+        <link rel="icon" type="image/png" href="/websiteIcon.png" />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
